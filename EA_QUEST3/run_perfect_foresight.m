ed70e97fe9750ebdf2af29da017145b5aa3797ad
MaximeBrn <37322244+MaximeBrn@users.noreply.github.com>
2022-04-07 12:15:25 +0200

    Try permanent shock

--- EA_QUEST3/run_perfect_foresight.m
+++ EA_QUEST3/run_perfect_foresight.m
@@ -13,20 +13,31 @@ cd([cd '/EA_QUEST3_rep']);
 
 dynare EA_Quest3_rep_perfect_foresight.mod
 
-gy_simul=oo_.endo_simul(find(strcmp(M_.endo_names,'E_GY')),:)';
-gg_simul=oo_.endo_simul(find(strcmp(M_.endo_names,'E_GG')),:)';
-figure
-t=1:1:42;
-plot(t,gy_simul,'LineWidth',2);
+GY0_vec = ones(1, length(oo_.endo_simul(1,:)))' * GY0;
+
+Y_baseline=cumprod(1+oo_.endo_simul(find(strcmp(M_.endo_names,'E_GY')),:))'-cumprod(1+GY0_vec);
+G_baseline=cumprod(1+oo_.endo_simul(find(strcmp(M_.endo_names,'E_GG')),:))'-cumprod(1+GY0_vec);
+fm_baseline=Y_baseline./G_baseline/GSN;
+FM_baseline=cumsum(Y_baseline)./cumsum(G_baseline)/GSN;
 
 figure
-t=1:1:42;
-plot(t,cumsum(gg_simul-GY0),'LineWidth',2);
+subplot(2,1,1)
+t=2:1:41;
+plot(t,Y_baseline(2:41),'LineWidth',2);
 
+subplot(2,1,2)
+t=2:1:41;
+plot(t,G_baseline(2:41),'LineWidth',2);
 
+figure
+subplot(2,1,1)
+t=2:1:41;
+plot(t,fm_baseline(2:41),'LineWidth',2);
 
+subplot(2,1,2)
+t=2:1:41;
+plot(t,FM_baseline(2:41),'LineWidth',2);
 
-fm=cumsum(gy_simul-GY0)./cumsum(gg_simul-GY0)/GSN;
 
 
 
